e116c469875313be68e6d787c7a761ccfdba4699
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2023-10-14 11:27:28 +0800

    add password info in community registration
    remove tracking for resources/lang

--- resources/lang/en/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'failed'   => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-    'tokenCSRF' => 'Your session has been expired, Please login again',
-];
--- resources/lang/ms/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'failed'   => 'Butiran ini tidak sepadan dengan rekod kami.',
-    'password' => 'Kata laluan yang diberikan tidak betul.',
-    'throttle' => 'Terlalu banyak percubaan log masuk. Sila cuba lagi dalam :seconds saat.',
-    'tokenCSRF' => 'Sesi anda telah tamat tempoh, Sila log masuk semula',
-];
