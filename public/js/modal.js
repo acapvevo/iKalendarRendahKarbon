2a3700cc4ec82dedf754e0bdc3bc1fd8ceccd432
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2023-12-04 13:57:46 +0800

    remove console log

--- public/js/modal.js
+++ public/js/modal.js
@@ -16,13 +16,11 @@ function initEventModal(modalElId, openFunc, closeFunc) {
 }
 
 function showLoadingModal() {
-    console.log(1);
     const modal = bootstrap.Modal.getOrCreateInstance('#loadingModal');
     modal.show();
 }
 
 function hideLoadingModal() {
-    console.log(2);
     const modal = bootstrap.Modal.getOrCreateInstance('#loadingModal');
     modal.hide();
 }
