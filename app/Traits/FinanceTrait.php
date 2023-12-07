fde16a3e6248e8e27363be4882c9833f1fe82dd9
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2023-12-07 12:35:59 +0800

    view finance info for admin in winner

--- app/Traits/FinanceTrait.php
+++ app/Traits/FinanceTrait.php
@@ -7,6 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 trait FinanceTrait
 {
+    public function getRules()
+    {
+        return [
+            'finance.account_number' => 'string|max:255',
+            'finance.account_name' => 'string|max:255',
+            'finance.bank' => 'numeric|exists:banks,code',
+            'bank_statement' => 'mimes:jpg,pdf,png|max:4096',
+        ];
+    }
+
     public function initFinance()
     {
         return new Finance;
@@ -27,5 +37,17 @@ trait FinanceTrait
     public function getBankList()
     {
         return DB::table('banks')->get();
+    }
+
+    public function saveFinance($community_id, $details, $bank_statement)
+    {
+        $finance = $this->getFinanceByCommunityID($community_id);
+
+        $finance->fill($details);
+
+        if ($bank_statement)
+            $finance->uploadBankStatement($bank_statement);
+
+        $finance->save();
     }
 }
