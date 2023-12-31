fc0e1615b8bc97257ce07ff602b3507bae309039
Cyber Panel <hamlala8@gmail.com>
2023-12-31 11:45:38 +0000

    add limit for category column

--- database/migrations/2023_12_11_093135_modify_category_tables.php
+++ database/migrations/2023_12_11_093135_modify_category_tables.php
@@ -14,39 +14,39 @@ class ModifyCategoryTables extends Migration
     public function up()
     {
         Schema::table('electrics', function (Blueprint $table) {
-            $table->decimal('usage')->change();
-            $table->decimal('charge')->change();
-            $table->decimal('carbon_emission')->change();
+            $table->decimal('usage', 12)->change();
+            $table->decimal('charge', 12)->change();
+            $table->decimal('carbon_emission', 12)->change();
         });
 
         Schema::table('waters', function (Blueprint $table) {
-            $table->decimal('usage')->change();
-            $table->decimal('charge')->change();
-            $table->decimal('carbon_emission')->change();
+            $table->decimal('usage', 12)->change();
+            $table->decimal('charge', 12)->change();
+            $table->decimal('carbon_emission', 12)->change();
         });
 
         Schema::table('recycles', function (Blueprint $table) {
-            $table->decimal('weight')->change();
-            $table->decimal('value')->change();
-            $table->decimal('carbon_emission')->change();
+            $table->decimal('weight', 12)->change();
+            $table->decimal('value', 12)->change();
+            $table->decimal('carbon_emission', 12)->change();
         });
 
         Schema::table('used_oil', function (Blueprint $table) {
-            $table->decimal('weight')->change();
-            $table->decimal('value')->change();
-            $table->decimal('carbon_emission')->change();
+            $table->decimal('weight', 12)->change();
+            $table->decimal('value', 12)->change();
+            $table->decimal('carbon_emission', 12)->change();
         });
 
         Schema::table('fabrics', function (Blueprint $table) {
-            $table->decimal('weight')->change();
-            $table->decimal('value')->change();
-            $table->decimal('carbon_emission')->change();
+            $table->decimal('weight', 12)->change();
+            $table->decimal('value', 12)->change();
+            $table->decimal('carbon_emission', 12)->change();
         });
 
         Schema::table('electronics', function (Blueprint $table) {
-            $table->decimal('weight')->change();
-            $table->decimal('value')->change();
-            $table->decimal('carbon_emission')->change();
+            $table->decimal('weight', 12)->change();
+            $table->decimal('value', 12)->change();
+            $table->decimal('carbon_emission', 12)->change();
         });
     }
 
