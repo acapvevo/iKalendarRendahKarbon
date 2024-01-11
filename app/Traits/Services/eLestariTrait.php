76c3674915d1f59e346f198fe5d141940f5496fc
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2024-01-11 14:53:21 +0800

    add elestari service

---
+++ app/Traits/Services/eLestariTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits\Services;
+
+use Illuminate\Support\Facades\Http;
+
+trait eLestariTrait
+{
+    private $url = 'https://elestari.pendidikankelestarianjohor.edu.my/api/';
+
+    public function getAuditTenagaData($year, $start_month, $finish_month)
+    {
+        $response = Http::withJson()->get($this->url . 'auditTenaga/bulanan',[
+            'tahun' => $year,
+            'bulan_mula' => $start_month,
+            'bulan_akhir' => $finish_month,
+            'pbt_kod' => '0102'
+        ]);
+
+        return $response->json('data');
+    }
+}
