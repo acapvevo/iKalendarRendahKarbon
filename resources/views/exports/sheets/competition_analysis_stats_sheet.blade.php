bf651836ffd49d7d74b4e7998465ec678646fbd0
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2024-01-11 14:27:07 +0800

    add monthly stats for competition analysis export

--- resources/views/exports/sheets/competition_analysis_stats_sheet.blade.php
+++ resources/views/exports/sheets/competition_analysis_stats_sheet.blade.php
@@ -20,6 +20,16 @@
                 kgCO<sub>2</sub></td>
         </tr>
     @endforeach
+    @foreach ($competition->getMonthRange() as $m => $month)
+        <tr>
+            @if ($m == $competition->getMonthRange()->keys()->first())
+                <th rowspan="{{ $competition->getMonthRange()->count() }}">{{ __('Total Carbon Emission By Month') }}</th>
+            @endif
+            <td>{{ $month->getName() }}</td>
+            <td>{{ number_format($calculation->total_carbon_emission_each_month[$month->id], 2) }}
+                kgCO<sub>2</sub></td>
+        </tr>
+    @endforeach
     <tr></tr>
     <tr></tr>
     <tr>
@@ -40,7 +50,17 @@
             @endif
             <td>{{ __($category->description) }}</td>
             <td>{{ number_format($stat->total_submission_each_type[$category->name], 2) }}
-                {{ __('Resident') }}</td>
+                {{ __('Resident(s)') }}</td>
+        </tr>
+    @endforeach
+    @foreach ($competition->getMonthRange() as $m => $month)
+        <tr>
+            @if ($m == $competition->getMonthRange()->keys()->first())
+                <th rowspan="{{ $competition->getMonthRange()->count() }}">{{ __('Total Submission By Month') }}</th>
+            @endif
+            <td>{{ $month->getName() }}</td>
+            <td>{{ number_format($stat->total_submission_each_month[$month->id], 2) }}
+                {{ __('Resident(s)') }}</td>
         </tr>
     @endforeach
 </table>
