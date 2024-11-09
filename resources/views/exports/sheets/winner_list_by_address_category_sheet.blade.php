8193b2529392c53e6f1beb0cc07be2bddfbde10f
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2024-11-09 13:24:30 +0800

    add columns in export winner for admin

--- resources/views/exports/sheets/winner_list_by_address_category_sheet.blade.php
+++ resources/views/exports/sheets/winner_list_by_address_category_sheet.blade.php
@@ -3,6 +3,10 @@
         <tr>
             <th>{{ __('Rank') }}</th>
             <th>{{ __('Name') }}</th>
+            <th>{{ __('Username') }}</th>
+            <th>{{ __('Identification Card Number') }}</th>
+            <th>{{ __('Identification Card') }} {{ __('Verified') }}?</th>
+            <th>{{ __('Email') }}</th>
             <th>{{ __('Phone Number') }}</th>
             <th>{{ __('Address') }}</th>
             <th>{{ __('Total Carbon Reduction') }}</th>
@@ -12,7 +16,11 @@
         @foreach ($submissions as $s => $submissionObj)
             <tr>
                 <td>{{ $s + 1 }}</td>
-                <td>{{ $submissionObj->community->name ?? $submissionObj->community->username }}</td>
+                <td>{{ $submissionObj->community->name }}</td>
+                <td>{{ $submissionObj->community->username }}</td>
+                <td>{{ $submissionObj->community->identification_number }}</td>
+                <td>{{ $submissionObj->community->isVerified ? __('Verified') : __('Not Verified') }}</td>
+                <td>{{ $submissionObj->community->email }}</td>
                 <td>{{ $submissionObj->community->getPhoneNumber() }}</td>
                 <td>{{ $submissionObj->community->address->getFullAddressInSingleLine() }}</td>
                 <td>{{ abs($submissionObj->calculation->total_carbon_reduction) }} kgCO<sub>2</sub></td>
